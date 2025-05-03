9e7b5cc89f6b855baaba9843094ebe027253c391
jeff <jeffersonking0511@gmail.com>
2025-05-03 17:26:12 +0530

    deploy changes

--- job-backend/src/app.controller.ts
+++ job-backend/src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get()  // This is correct usage of @Get() without any parameters
   getHello(): string {
     return this.appService.getHello();
   }
