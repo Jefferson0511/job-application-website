39ae94040caa18fef5d07e7e47d25a262d42ee23
jeff <jeffersonking0511@gmail.com>
2025-05-05 20:49:24 +0530

    backend deploy

--- job-backend/src/main.ts
+++ job-backend/src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Connection } from 'typeorm';
+import 'reflect-metadata';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
